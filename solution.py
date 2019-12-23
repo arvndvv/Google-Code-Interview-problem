75728ef604d205428d22b1d3d87e0040f0fdd9d4
Aravind V V <aravind16498@gmail.com>
2019-12-23 14:23:50 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,32 @@
 '''
-
                            Google Coding Interview Problem
                                   Difficulty:easy
         Given a list of words and two words word1 and word2, return the shortest
         distance between these two words in the list
-
 '''
+def check(w1,w2):
+    d=n
+    wl1=[]
+    wl2=[]
+    wr1=words.count(w1)
+    wr2=words.count(w2)
+    for i in range(n):
+        if(words[i]==w1):
+            wl1.append(i)
+            
+    
+    for i in range(n):
+
+        if(words[i]==w2):
+            wl2.append(i)
+            
+    print(wl1,':',wl2)
+    for i in range(wr1):
+        for j in range(wr2):
+            dnew=abs(wl1[i]-wl2[j])
+            if(d>dnew):
+                d=dnew
+    return d;
 ch='y'
 while(ch=='y'):
     words=[]
@@ -31,9 +52,7 @@ while(ch=='y'):
             break
         print("Word not in list!")
         continue
-    i1=words.index(w1)+1
-    i2=words.index(w2)+1
-    d=abs(i1-i2)
+    d=check(w1,w2)
     print("Shortest distance between them is: ",d)
     print("Do you wanna try again?(y/n)")
     ch=input()
